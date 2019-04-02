83a4ebeb31a1cfad88d1a08a6984a1b313bd16d4
nilsroesel <nils.roesel@gmail.com>
2019-04-02 23:15:15 +0200

    Add modern barbed wire fence

--- src/app/home/fences/index.ts
+++ src/app/home/fences/index.ts
@@ -1,9 +1,11 @@
 import { NORMAL_FENCE  as normal } from './normal.fence';
 import { LASER_FENCE as laser } from './laser.fence';
 import { WIRE_MESH_FENCE as wireMesh } from './wire-mesh.fence';
+import { MODERN_BARBED_WIRE as modernBarbedWire } from './modern-barbed-wire';
 
 export const NORMAL_FENCE = normal;
 export const WIRE_MESH_FENCE = wireMesh;
 export const LASER_FENCE = laser;
+export const MODERN_BARBED_WIRE = modernBarbedWire;
 
-export const FENCES = [NORMAL_FENCE, LASER_FENCE, WIRE_MESH_FENCE];
+export const FENCES = [NORMAL_FENCE, LASER_FENCE, WIRE_MESH_FENCE, MODERN_BARBED_WIRE];
---
+++ src/app/home/fences/modern-barbed-wire.ts
@@ -0,0 +1,9 @@
+import { FenceAsset } from '../../model/fence-asset';
+import { Feature } from '../../model/feature';
+
+export const MODERN_BARBED_WIRE: FenceAsset = {
+    name: 'Fence.ModernBarbedWire',
+    path: 'assets/fences/modern-barbed-wire-sprite.png',
+    feature: Feature.FENCE_MODERN_BARBED_WIRE,
+    price: 200
+};
