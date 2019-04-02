25ef725f9b7d9d6ff7d2ec1d76463225640a0e21
nilsroesel <nils.roesel@gmail.com>
2019-04-03 00:37:21 +0200

    Add berlin wall

---
+++ src/app/home/fences/berlin-wall.fence.ts
@@ -0,0 +1,9 @@
+import { FenceAsset } from '../../model/fence-asset';
+import { Feature } from '../../model/feature';
+
+export const BERLIN_WALL: FenceAsset = {
+    name: 'Fence.BerlinWall',
+    path: 'assets/fences/berlin-wall-sprite.png',
+    feature: Feature.FENCE_BERLIN_WALL,
+    price: 500
+};
--- src/app/home/fences/index.ts
+++ src/app/home/fences/index.ts
@@ -3,11 +3,13 @@ import { LASER_FENCE as laser } from './laser.fence';
 import { WIRE_MESH_FENCE as wireMesh } from './wire-mesh.fence';
 import { MODERN_BARBED_WIRE as modernBarbedWire } from './modern-barbed-wire.fence';
 import { RUSTY_BARBED_WIRE as rustyBarbedWire } from './rusty-barbed-wire.fence';
+import { BERLIN_WALL as berlinWall } from './berlin-wall.fence';
 
 export const NORMAL_FENCE = normal;
 export const WIRE_MESH_FENCE = wireMesh;
 export const LASER_FENCE = laser;
 export const MODERN_BARBED_WIRE = modernBarbedWire;
 export const RUSTY_BARBED_WIRE = rustyBarbedWire;
+export  const BERLIN_WALL = berlinWall;
 
-export const FENCES = [NORMAL_FENCE, LASER_FENCE, WIRE_MESH_FENCE, RUSTY_BARBED_WIRE, MODERN_BARBED_WIRE];
+export const FENCES = [NORMAL_FENCE, LASER_FENCE, WIRE_MESH_FENCE, RUSTY_BARBED_WIRE, MODERN_BARBED_WIRE, BERLIN_WALL];
