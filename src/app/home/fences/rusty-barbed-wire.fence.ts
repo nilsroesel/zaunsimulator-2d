f2ebac54079ec8efe013883b2dc0007d38022911
nilsroesel <nils.roesel@gmail.com>
2019-04-02 23:50:23 +0200

    Add rusty barbed wire fence

---
+++ src/app/home/fences/rusty-barbed-wire.fence.ts
@@ -0,0 +1,9 @@
+import { FenceAsset } from '../../model/fence-asset';
+import { Feature } from '../../model/feature';
+
+export const RUSTY_BARBED_WIRE: FenceAsset = {
+    name: 'Fence.RustyBarbedWire',
+    path: 'assets/fences/rusty-barbed-wire-sprite.png',
+    feature: Feature.FENCE_RUSTY_BARBED_WIRE,
+    price: 75
+};
