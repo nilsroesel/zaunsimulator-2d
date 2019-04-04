5efd9a5c51752e97440464913236329bd21684c2
nilsroesel <nils.roesel@gmail.com>
2019-04-04 15:56:26 +0200

    Add hoarding fence

---
+++ src/app/home/fences/hoarding.fence.ts
@@ -0,0 +1,9 @@
+import { FenceAsset } from '../../model/fence-asset';
+import { Feature } from '../../model/feature';
+
+export const HOARDING: FenceAsset = {
+    name: 'Fence.Hoarding',
+    path: 'assets/fences/bauzaun-sprite.png',
+    feature: Feature.FENCE_HOARDING,
+    price: 100
+};
--- src/app/home/fences/index.ts
+++ src/app/home/fences/index.ts
@@ -4,12 +4,14 @@ import { WIRE_MESH_FENCE as wireMesh } from './wire-mesh.fence';
 import { MODERN_BARBED_WIRE as modernBarbedWire } from './modern-barbed-wire.fence';
 import { RUSTY_BARBED_WIRE as rustyBarbedWire } from './rusty-barbed-wire.fence';
 import { BERLIN_WALL as berlinWall } from './berlin-wall.fence';
+import { HOARDING as hoardingFence } from './hoarding.fence';
 
 export const NORMAL_FENCE = normal;
 export const WIRE_MESH_FENCE = wireMesh;
 export const LASER_FENCE = laser;
 export const MODERN_BARBED_WIRE = modernBarbedWire;
 export const RUSTY_BARBED_WIRE = rustyBarbedWire;
-export  const BERLIN_WALL = berlinWall;
+export const BERLIN_WALL = berlinWall;
+export const HOARDING_FENCE = hoardingFence;
 
-export const FENCES = [NORMAL_FENCE, LASER_FENCE, WIRE_MESH_FENCE, RUSTY_BARBED_WIRE, MODERN_BARBED_WIRE, BERLIN_WALL];
+export const FENCES = [NORMAL_FENCE, RUSTY_BARBED_WIRE, WIRE_MESH_FENCE, HOARDING_FENCE, MODERN_BARBED_WIRE, LASER_FENCE, BERLIN_WALL];
